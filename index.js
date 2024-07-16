01d02cd8547e01b2281aacd5ada2bc9075d724e0
Joseph Brendan <jbrendan86@gmail.com>
2024-07-16 20:06:31 +0100

    recent

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const hexadecimalNumberSystem = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "
 let button = document.getElementById("button")
 let firstColorBox = document.getElementById("first-color-box")
 let firstHexCode = document.getElementById("first-hex-code")
+let secondColorBox = document.getElementById("second-color-box")
+let secondHexCode = document.getElementById("second-hex-code")
+let thirdColorBox = document.getElementById("third-color-box")
+let thirdHexCode = document.getElementById("third-hex-code")
+
 
 function generateRandomNumber(){
     let randomNumber = Math.floor(Math.random() * hexadecimalNumberSystem.length) 
@@ -9,6 +14,7 @@ function generateRandomNumber(){
 }
 
 button.addEventListener("click", function(){
+    // For First Color Box
     let hexCodeForFirstBox = "#"
     for(let myFirstLoop = 0; myFirstLoop < 6; myFirstLoop++){
         hexCodeForFirstBox += hexadecimalNumberSystem[generateRandomNumber()]
@@ -16,8 +22,21 @@ button.addEventListener("click", function(){
     firstColorBox.style.backgroundColor = hexCodeForFirstBox
     firstHexCode.textContent = hexCodeForFirstBox
 
-    
+    // For Second Color Box
+    let hexCodeForSecondBox = "#"
+    for(let mySecondLoop = 0; mySecondLoop < 6; mySecondLoop++){
+        hexCodeForSecondBox += hexadecimalNumberSystem[generateRandomNumber()]
+    }
+    secondColorBox.style.backgroundColor = hexCodeForSecondBox
+    secondHexCode.textContent = hexCodeForSecondBox
 
+    // For Third Color Box
+    let hexCodeForThirdBox = "#"
+    for(let myThirdLoop = 0; myThirdLoop < 6; myThirdLoop++){
+        hexCodeForThirdBox += hexadecimalNumberSystem[generateRandomNumber()]
+    }
+    thirdColorBox.style.backgroundColor = hexCodeForThirdBox
+    thirdHexCode.textContent = hexCodeForThirdBox
 })
 
 
